b517177e38fea3958c9d27d2541ae0f81794e438
Lucas <lucashoffmann99@gmail.com>
2022-06-30 10:34:51 -0300

    Adding 'hello world' in 10 different ways

---
+++ script.js
@@ -0,0 +1,28 @@
+const app = document.querySelector("#app");
+app.innerHTML = `
+<header>
+    <h1> hello world </h1>
+</header>
+<p> hello world </p>
+<section>
+    <h2> hello world </h2>
+</section>
+<ul>
+    <li> hello world </li>
+</ul>
+<ol>
+    <li> hello world </li>
+</ol>
+<aside> 
+    <h6> hello world </h6>
+</aside>
+<article> 
+    <p> hello world </p>
+</article>
+<h3> hello world </h3>
+<h4> hello world </h4>
+<footer>
+    <h5> hello world </h5>
+</footer>
+
+`;
